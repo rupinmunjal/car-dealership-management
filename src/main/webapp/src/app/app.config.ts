a2fc455608a2935fe9658c52990a4a60af040cca
Rupin Munjal <157275712+rupinmunjal@users.noreply.github.com>
2026-01-09 14:15:00 -0500

    feat: enable Material animations provider in app config

--- src/main/webapp/src/app/app.config.ts
+++ src/main/webapp/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
@@ -8,6 +9,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations()
   ]
 };
