2be0bcf054482eb7ff8ea512d5a58ebcc2896751
Rupin Munjal <157275712+rupinmunjal@users.noreply.github.com>
2026-01-20 09:00:00 -0500

    feat: add cars list component with Material table data source

--- src/main/webapp/src/app/pages/cars-list/cars-list.ts
+++ src/main/webapp/src/app/pages/cars-list/cars-list.ts
@@ -3,12 +3,20 @@ import { CarService } from '../../services/car';
 import { AuthService } from '../../services/auth';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-cars-list',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,
+    MatTableModule, MatCardModule, MatButtonModule,
+    MatIconModule, MatChipsModule, MatTooltipModule],
   templateUrl: './cars-list.html',
-  styleUrls: ['./cars-list.css'],
+  styleUrl: './cars-list.css',
 })
 export class CarsList implements OnInit {
 
@@ -25,6 +33,12 @@ export class CarsList implements OnInit {
     return this.authService.isAdmin();
   }
 
+  get displayedColumns(): string[] {
+    return this.isAdmin
+      ? ['id', 'make', 'model', 'modelYear', 'actions']
+      : ['id', 'make', 'model', 'modelYear'];
+  }
+
   ngOnInit(): void {
     this.loadCars();
     this.loadBrandsCount();
@@ -32,37 +46,22 @@ export class CarsList implements OnInit {
 
   loadCars() {
     this.carService.getAll().subscribe({
-      next: (data) => {
-        this.cars = data;
-        this.cd.detectChanges(); // update view
-      },
-      error: () => {
-        this.cars = [];
-        this.cd.detectChanges(); // update view
-      }
+      next: (data) => { this.cars = data; this.cd.detectChanges(); },
+      error: () => { this.cars = []; this.cd.detectChanges(); }
     });
   }
 
   loadBrandsCount() {
     this.carService.getUniqueBrandsCount().subscribe({
-      next: (data) => {
-        this.uniqueBrandsCount = data.uniqueBrandsCount;
-        this.cd.detectChanges();
-      },
-      error: () => {
-        this.uniqueBrandsCount = 0;
-        this.cd.detectChanges();
-      }
+      next: (data) => { this.uniqueBrandsCount = data.uniqueBrandsCount; this.cd.detectChanges(); },
+      error: () => { this.uniqueBrandsCount = 0; this.cd.detectChanges(); }
     });
   }
 
   deleteCar(id: number) {
-    if (confirm("Are you sure you want to delete this car?")) {
+    if (confirm('Are you sure you want to delete this car?')) {
       this.carService.deleteCar(id).subscribe({
-        next: () => {
-          this.loadCars();
-          this.loadBrandsCount();
-        },
+        next: () => { this.loadCars(); this.loadBrandsCount(); },
         error: () => {}
       });
     }
